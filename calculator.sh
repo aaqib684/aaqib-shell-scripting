0b2d6a72c9c712cf614aa6d0466bd4e18053b26d
Aaqib <shaikaaqib684.com>
2025-03-03 16:24:02 +0530

    Re-added subtraction feature in calculator.sh

--- calculator.sh
+++ calculator.sh
@@ -13,6 +13,16 @@ read num1
 echo "Enter second number:"
 read num2
 
+
 result=$((num1 + num2))
 echo "The sum is: $result"
 
+# Substraction
+sub=$((num1 - num2 ))
+echo "The subtraction is: $sub"
+
+# Mutiplication
+
+
+result=$((num1*num2))
+echo "The multiplication is: $result"
