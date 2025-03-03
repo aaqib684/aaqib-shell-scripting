3987a32833c42d3689e489fbb48e2998ddee124c
Aaqib <shaikaaqib684.com>
2025-03-03 12:31:05 +0530

    Added calculator.sh script

---
+++ calculator.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+## Author : Aquib
+## Date : 02/03/2024
+##Git practic
+
+#!/bin/bash
+
+set -x
+
+echo "Enter first number:"
+read num1
+echo "Enter second number:"
+read num2
+
+result=$((num1 + num2))
+echo "The sum is: $result"
+
